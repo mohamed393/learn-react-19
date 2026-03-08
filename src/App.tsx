30a6b2bb7c0a89c05dfbcce2c99311a6d8e0169a
MohamedMosleh <mohamed.mosleh@hypercell.net>
2026-03-08 13:55:12 +0200

    react rules ( Components and Hooks must be pure and predictable)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+* Components and Hooks must be pure
+* So in React, “idempotent” really means predictable on re-render.
+*************Sample one Bad function Clock() {
+  const time = new Date(); // 🔴 Bad: always returns a different result!
+  return <span>{time.toLocaleString()}</span>
+  * This doesn’t mean you shouldn’t use non-idempotent functions like new Date()
+  * at all – you should just avoid using them during render. In this case, we can synchronize the latest date to this component using an Effect:
+
+*************** Sample one solution
+* import { useState, useEffect } from 'react';
+
+function useTime() {
+  // 1. Keep track of the current date's state. `useState` receives an initializer function as its
+  //    initial state. It only runs once when the hook is called, so only the current date at the
+  //    time the hook is called is set first.
+  const [time, setTime] = useState(() => new Date());
+
+  useEffect(() => {
+    // 2. Update the current date every second using `setInterval`.
+    const id = setInterval(() => {
+      setTime(new Date()); // ✅ Good: non-idempotent code no longer runs in render
+    }, 1000);
+    // 3. Return a cleanup function so we don't leak the `setInterval` timer.
+    return () => clearInterval(id);
+  }, []);
+
+  return time;
+}
+
+export default function Clock() {
+  const time = useTime();
+  return <span>{time.toLocaleString()}</span>;
+}
+App.js
+}
+___________________________________________________________________________________________________
+* **** sample two Local mutation
+const items = []; // 🔴 Bad: created outside of the component
+function FriendList({ friends }) {
+  for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    items.push(
+      <Friend key={friend.id} friend={friend} />
+    ); // 🔴 Bad: mutates a value created outside of render
+  }
+  return <section>{items}</section>;
+}
+* ***** sample two solution
+* function FriendList({ friends }) {
+  const items = []; // ✅ Good: locally created
+  for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    items.push(
+      <Friend key={friend.id} friend={friend} />
+    ); // ✅ Good: local mutation is okay
+  }
+  return <section>{items}</section>;
+}
+________________________________________________________________________________________________
+****** sample three Don’t mutate Props
+function Post({ item }) {
+  item.url = new Url(item.url, base); // 🔴 Bad: never mutate props directly
+  return <Link url={item.url}>{item.title}</Link>;
+}
+function Post({ item }) {
+  const url = new Url(item.url, base); // ✅ Good: make a copy instead
+  return <Link url={url}>{item.title}</Link>;
+}
+****************************Return values and arguments to Hooks are immutable
+function useIconStyle(icon) {
+  const theme = useContext(ThemeContext);
+  if (icon.enabled) {
+    icon.className = computeStyle(icon, theme); // 🔴 Bad: never mutate hook arguments directly
+  }
+  return icon;
+}
+function useIconStyle(icon) {
+  const theme = useContext(ThemeContext);
+  const newIcon = { ...icon }; // ✅ Good: make a copy instead
+  if (icon.enabled) {
+    newIcon.className = computeStyle(icon, theme);
+  }
+  return newIcon;
+}
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>The explanation with samples are exist in the comments top of the component</p>
     </>
   )
 }
